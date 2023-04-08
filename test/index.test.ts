016aaf1795040c26044c93d0dd7648697e9f68a2
Siraphat Phetsakun <siraphat.phetsakun@gmail.com>
2023-04-09 01:21:23 +0700

    refactor: fix word conlection to collection

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,27 +1,27 @@
 import merge from '../src'
 
 describe('testing merge function', () => {
-  test('empty conlection', () => {
+  test('empty collection', () => {
     expect(merge([], [])).toEqual([])
   })
 
-  test('empty conlection one', () => {
+  test('empty collection one', () => {
     expect(merge([], [3, 2])).toEqual([2, 3])
   })
 
-  test('empty conlection two', () => {
+  test('empty collection two', () => {
     expect(merge([3, 2, 5], [])).toEqual([2, 3, 5])
   })
 
-  test('sort conlection', () => {
+  test('sort collection', () => {
     expect(merge([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6])
   })
 
-  test('not sort conlection', () => {
+  test('not sort collection', () => {
     expect(merge([5, 9, 3], [9, 6])).toEqual([3, 5, 6, 9, 9])
   })
 
-  test('same conlection', () => {
+  test('same collection', () => {
     expect(merge([1, 2], [1, 2])).toEqual([1, 1, 2, 2])
   })
 })
